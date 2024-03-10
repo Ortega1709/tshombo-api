b09755ec57bec814b7a631013c6da0bd6f0fd17f
ortega <kabweortega@gmail.com>
2024-03-11 00:50:08 +0200

    Initial commit

---
+++ src/main/java/com/ortega/tshomboapi/TshomboApiApplication.java
@@ -0,0 +1,13 @@
+package com.ortega.tshomboapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshomboApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TshomboApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ortega/tshomboapi/TshomboApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.tshomboapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TshomboApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
