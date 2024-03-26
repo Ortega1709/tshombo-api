e14172b3aad42fd605bdb2fe5165395abb979842
ortega <kabweortega@gmail.com>
2024-03-26 13:07:22 +0200

    Refactor: Modify User Model to implement UserDetails Interface

--- src/main/java/com/ortega/tshomboapi/model/Role.java
+++ src/main/java/com/ortega/tshomboapi/model/Role.java
@@ -1,23 +1,9 @@
 package com.ortega.tshomboapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+public enum Role {
 
-import java.util.UUID;
+    ADMIN,
+    USER
 
-@Entity
-@Getter
-@Setter
-@Builder
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID roleId;
-    private String role;
 }
+
--- src/main/java/com/ortega/tshomboapi/model/User.java
+++ src/main/java/com/ortega/tshomboapi/model/User.java
@@ -2,8 +2,13 @@ package com.ortega.tshomboapi.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -13,16 +18,50 @@ import java.util.UUID;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID userId;
-    private String name;
+    private String username;
     private String email;
     private String password;
-    private final LocalDateTime createdAt = LocalDateTime.now();
 
-    @OneToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "roleId")
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
